4c44accc97c7b32c870c73b5d2eb5d7ae417e168
Nick Hijden <nick.vander.hijden@cgi.com>
2026-09-21 17:19:24 +0200

    Avoid conflicting API headers in reminder cron

--- supabase/reminders-setup.sql
+++ supabase/reminders-setup.sql
@@ -32,7 +32,6 @@ grant all on public.homeboard_reminder_log to service_role;
 --     headers := jsonb_build_object(
 --       'Content-Type', 'application/json',
 --       'apikey', (select decrypted_secret from vault.decrypted_secrets where name = 'homeboard_publishable_key'),
---       'Authorization', 'Bearer ' || (select decrypted_secret from vault.decrypted_secrets where name = 'homeboard_publishable_key'),
 --       'x-homeboard-cron-secret', (select decrypted_secret from vault.decrypted_secrets where name = 'homeboard_cron_secret')
 --     ),
 --     body := '{}'::jsonb
